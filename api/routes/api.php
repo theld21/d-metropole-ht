41be3690e479b64eade1a05f9040dbc0e2d6bcbe
ldt <lam@MacBook-Pro.local>
2024-03-14 08:23:23 +0700

    update laravel

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Client\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
